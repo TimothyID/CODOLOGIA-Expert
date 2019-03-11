4d28b329cfe0cedee1f38f98f453fd2df6ea22cb
Paasonen Timothy <StrqnDed@protonmail.com>
2019-03-11 16:08:09 +0000

    Update gui.java

--- gui.java
+++ gui.java
@@ -30,9 +30,6 @@ public class Main extends Application {
         primaryStage.setTitle("CODOLOGIA");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
-
 }
     public static void main(String[] args) {
         launch(args);
